76c0ab91488b72dc8764e3b746c8a480ff42d8a5
Dan Cruson <cruson.web@gmail.com>
2014-11-05 08:38:11 -0500

    Clean up of code comments
    Clean up of code comments and Wrong extension

--- eosnotify.php
+++ eosnotify.php
@@ -1,6 +1,9 @@
 <?php
 
 /**
+ * @package		Joomla.Plugin
+ * @subpackage  Quickicon.eosnotify
+ *
  * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -9,12 +12,9 @@ defined('_JEXEC') or die;
 
 /**
  * Joomla! udpate notification plugin
- *
- * @package		Joomla.Plugin
- * @subpackage	Quickicon.Joomla
- * @since		2.5
+ * @since  2.5
  */
-class plgQuickiconEosnotify extends JPlugin
+class PlgQuickiconEosnotify extends JPlugin
 {
 	/**
 	 * Constructor
@@ -34,16 +34,15 @@ class plgQuickiconEosnotify extends JPlugin
 	}
 
 	/**
-	 * This method is called when the Quick Icons module is constructing its set
-	 * of icons. You can return an array which defines a single icon and it will
-	 * be rendered right after the stock Quick Icons.
+	 * Returns an icon definition for an icon which looks for extensions updates
+ 	 * via AJAX and displays a notification when such updates are found.
 	 *
-	 * @param  $context  The calling context
+	 * @param  string  $context  The calling context
 	 *
-	 * @return array A list of icon definition associative arrays, consisting of the
-	 *				 keys link, image, text and access.
+	 * @return  array  A list of icon definition associative arrays, consisting of the
+	 *				   keys link, image, text and access.
 	 *
-	 * @since       2.5
+	 * @since   2.5
 	 */
 	public function onGetIcons($context)
 	{
